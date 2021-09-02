486ec4ff796ba4d94f0dbf1c9489c839085e514e
guochao <463561248@qq.com>
2021-09-02 22:06:06 +0800

    demo

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import SiteRoot from './SiteRoot.vue'
 import createDemoRouter from './routes/router'
+import { routes } from './routes/routes'
 
 const app = createApp(SiteRoot)
 const routes = []
---
+++ demo/routes/routes.js
@@ -0,0 +1,8 @@
+
+export const routes = [
+  {
+    name: 'home',
+    path: '/home',
+    component: () => import('../pages/home/index.vue')
+  }
+]
